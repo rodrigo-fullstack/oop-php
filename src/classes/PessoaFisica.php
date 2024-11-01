74de0de976ce98df18fb34942f9a2d2e5645fe21
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-01 20:31:13 -0300

    docs: Comentário sobre Classes

--- src/classes/PessoaFisica.php
+++ src/classes/PessoaFisica.php
@@ -1,5 +1,7 @@
 <?php
 
+//Classe é uma forma que conterá um objeto
+//Em outras palavras é um projeto contendo todos os atributos e métodos que um objeto possuirá em sua instância
 class PessoaFisica{
     public $nome;
     public $idade;
