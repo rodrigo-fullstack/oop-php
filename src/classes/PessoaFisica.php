60a6c97bbe84fdf080220c6232aa6dbaf84a2e58
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-01 20:32:03 -0300

    docs: Comentando atributos da classe

--- src/classes/PessoaFisica.php
+++ src/classes/PessoaFisica.php
@@ -3,7 +3,10 @@
 //Classe é uma forma que conterá um objeto
 //Em outras palavras é um projeto contendo todos os atributos e métodos que um objeto possuirá em sua instância
 class PessoaFisica{
+
+    //Aqui vão os métodos e atributos
     public $nome;
+    //Atributos são características ou elementos da classe
     public $idade;
 
 }
