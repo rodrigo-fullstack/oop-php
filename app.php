ae668132e05828797207b976f2013ba51db40fbe
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-02 10:23:46 -0300

    feat (method): Testando métodos

--- app.php
+++ app.php
@@ -1,25 +1,40 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
-//Usando namespace definido com nome diferente
-//use Rodrigo\OopPhp\Classes\PessoaFisica as PesFis;
-use Rodrigo\OopPhp\Classes\PessoaFisica;
+// Usando namespace definido com nome diferente
+// use Rodrigo\OopPhp\Classes\PessoaFisica as PesFis;
+// use Rodrigo\OopPhp\Classes\PessoaFisica;
+
+use Rodrigo\OopPhp\Metodos\Banco;
 
 //$pessoa = new \Rodrigo\OopPhp\Classes\PessoaFisica(); Instanciando Objeto com namespace
 
 //$pessoa = new PesFis(); Instanciando objeto com nome personalizado
 
 
-//Criando objeto (instanciando)
+/*
+Criando objeto (instanciando)
 $pessoa = new PessoaFisica();
 
-//Definindo atributos públicos
+Definindo atributos públicos
 $pessoa -> nome = "Rodrigo";
 $pessoa -> idade = 18;
 $pessoa -> data_nasc = 110229;
 $pessoa -> endereco = "Rua das Bromélias";
+*/
+
+//Testando Métodos
+$banco = new Banco();
+
+$banco-> depositarDinheiro(100);
+echo "Saldo: " . $banco-> capturarSaldo();
+
+$banco-> sacarDinheiro(20);
+echo "\nSaldo: " . $banco-> capturarSaldo();
+
 
 //Melhor vardumper, do Symphony
-dump($pessoa);
 //No dump, + significa público
 //No dump, - significa privado
+
+//dump($pessoa);
---
+++ src/Metodos/Banco.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rodrigo\OopPhp\Metodos;
+
+class Banco{
+    //Atributos privados não são acessíveis diretamente com a seta -> para nome_objeto -> atributo;
+    private $saldo;
+
+    //Acessamos com métodos públicos
+    public function capturarSaldo(){
+        return $this -> saldo;
+    }
+
+    //Retira do saldo a quantia especificada
+    public function sacarDinheiro($dinheiro) {
+
+        //Verificar se a quantia digitada é menor que o salário
+        if($dinheiro <= $this -> saldo){
+            $this -> saldo -= $dinheiro;
+            return true;
+        }
+
+        //Verificar se
+        return false;
+
+
+    }
+
+    //Adiciona ao saldo a quantia necessitada
+    public function depositarDinheiro($dinheiro){
+        $this -> saldo += $dinheiro;
+        return true;
+    }
+
+}
